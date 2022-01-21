2b133baf7b7d44e953447d29e3c373f907d77c49
Piotr Hondra <piotr.hondra@gmail.com>
2022-01-22 00:26:48 +0100

    added fixtures

--- tests/test_NaiveBayes.py
+++ tests/test_NaiveBayes.py
@@ -1,6 +1,19 @@
+import numpy as np
+import pandas as pd
 import pytest
 
 
 @pytest.fixture
-def y():
-    pass
+def data():
+    data = pd.read_csv('data/agaricus-lepiota.data', header=None).to_numpy()
+    return data
+
+
+@pytest.fixture
+def X_y(data):
+    y, X = np.split(data, [1], axis=1)
+    return X, y
+
+
+def test_init():
+    assert True
