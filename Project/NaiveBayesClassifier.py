3e50c93a102b9e5884cb2e8fddabb44f9d5cc188
Piotr Hondra <piotr.hondra@gmail.com>
2022-01-22 14:50:01 +0100

    added predictions for NB, evaluated

--- Project/NaiveBayesClassifier.py
+++ Project/NaiveBayesClassifier.py
@@ -1,47 +1,5 @@
-# import pandas as pd
-# import seaborn as sns
-# import matplotlib.pyplot as plt
 import numpy as np
 from collections import defaultdict, Counter
-
-# import math
-# import os
-
-# def get_label_indices(y):
-#     label_indices = defaultdict(list)
-#     for idx, label in enumerate(y):
-#         label_indices[label].append(idx)
-#     return label_indices
-
-# def get_prior(label_indices):
-#     prior = {label: len(indices) for label, indices in label_indices.items()}
-#     total = sum(prior.values())
-#     for label in prior:
-#         prior[label] /= total
-#     return prior
-
-# def get_likelihood(x, gausian_params, label_indices):
-#     likelihoods = []
-#     for x in X_test.to_numpy():
-#         likelihood = {}
-#         for label, indices in label_indices.items():
-#             likelihood[label] = 0
-#             # gausian_params[label] = [f1,f2,f3]
-#             for feature, (mean, std) in zip(list(x), list(gausian_params[label])):
-#                 likelihood[label] += math.log(
-#                     gaussian_formula(std, mean, feature) + math.pow(10, -10)
-#                 )
-#         likelihoods.append(likelihood)
-#     return likelihoods
-
-# def get_posterior(prior, likelihood):
-#     post = {}
-#     for label in prior.keys():
-#         post[label] = math.log(prior[label]) + likelihood[label]
-#     return post
-
-# def predict(posterior: dict):
-#     return max(posterior, key=posterior.get)
 
 
 class NaiveBayes:
@@ -91,3 +49,8 @@ class NaiveBayes:
                     posterior[label] /= sum_posterior
             posteriors.append(posterior.copy())
         return posteriors
+
+    def predict(self, posterior: list):
+        result = [max(prediction, key=prediction.get)
+                  for prediction in posterior]
+        return result
