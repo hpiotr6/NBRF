54565bcfd678b3da0b43038c563c3f25d2735d07
Piotr Hondra <piotr.hondra@gmail.com>
2022-01-25 15:33:35 +0100

    changed y type to np.array, added fit for RF

--- Project/BayesianRandomForest.py
+++ Project/BayesianRandomForest.py
@@ -1,25 +1,37 @@
-from NaiveBayesClassifier import NaiveBayes
+from .NaiveBayesClassifier import NaiveBayes
 import numpy as np
 
 
 class RandomForest:
-    def __init__(self, n_estimators) -> None:
-        self.clf = NaiveBayes()
+    def __init__(self, n_estimators, feature_bagging=False) -> None:
+        self.clf = NaiveBayes
         self.n_estimators = n_estimators
+        self.feature_bagging = feature_bagging
+        self._clfs = []
 
     @staticmethod
-    def feature_bag(self, X):
+    def feature_bag(X, y):
         n_rows, n_cols = X.shape
         samples_cols = np.random.choice(
             a=n_cols, size=int(np.sqrt(n_cols)), replace=True)
-        return X[:, samples_cols]
+        return X[:, samples_cols], y
 
     @staticmethod
-    def bag(self, X, y):
+    def bag(X, y):
         n_rows, n_cols = X.shape
         samples_rows = np.random.choice(a=n_rows, size=n_rows, replace=True)
         return X[samples_rows], y[samples_rows]
 
     def fit(self, X, y):
-        for i in range(self.n_estimators):
+        clfs = []
+        for _ in range(self.n_estimators):
+            clf = self.clf()
             _X, _y = self.bag(X, y)
+            clf.fit(_X, _y)
+            clfs.append(clf)
+        self.clfs = clfs
+        return self.clfs
+
+    # def predict(self, X):
+    #     for clf in self.clfs:
+    #         prediction = clf.predict(X)
