6dd9302409453d11034985cda5bab1f49efaa945
Piotr Hondra <piotr.hondra@gmail.com>
2022-01-26 16:57:59 +0100

    final changes

--- Project/utils.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from pathlib import Path
-import numpy as np
-
-data = pd.read_csv('data/agaricus-lepiota.data', header=None).to_numpy()
-
-y, X = np.split(data, [1], axis=1)
-print(y.shape)
-print(X.shape)
